8e65522b3335cc5ed2e6e5feb3eb5e521d69fce9
Annika Gilewski <annika.gilewski@gmx.de>
2026-09-04 17:27:09 +0200

    Third commit snowman ACSII

--- snowman.py
+++ snowman.py
@@ -1,5 +1,36 @@
 import random
 
+# Snowman ASCII Art stages
+STAGES = [
+     # Stage 0: Full snowman
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+     """,
+     # Stage 1: Bottom part starts melting
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 2: Only the head remains
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     """,
+     # Stage 3: Snowman completely melted
+     """
+      ___  
+     /___\\ 
+     """
+ ]
+
+
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
@@ -11,14 +42,38 @@ def get_random_word():
 
 def play_game():
     secret_word = get_random_word()
-    print("Welcome to Snowman Meltdown!")
-    print("Secret word selected: " + secret_word)  # for testing, later remove this line
+    mistakes = 0
+    guessed_letters = []
 
-    # TODO: Build your game loop here.
-    # For now, simply prompt the user once:
-    guess = input("Guess a letter: ").lower()
-    print("You guessed:", guess)
+    print("Welcome to Snowman Meltdown!")
+
+    while mistakes < len(STAGES) - 1:
+        display_game_state(mistakes, secret_word, guessed_letters)
+
+        guess = input("Guess a letter: ").lower()
+
+        guessed_letters.append(guess)
+
+        if guess not in secret_word:
+            mistakes += 1
+
+def display_game_state(mistakes, secret_word, guessed_letters):
+    print(STAGES[mistakes])
+
+    displayed_word = ""
+
+    for letter in secret_word:
+        if letter in guessed_letters:
+            displayed_word += letter + " "
+        else:
+            displayed_word += "_ "
+
+    print("Word:", displayed_word)
+
 
 
 if __name__ == "__main__":
     play_game()
+
+
+
